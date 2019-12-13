c65f438a893eb812dae9cf238167fd8e31873313
root <root@ip-172-31-43-149.ap-northeast-2.compute.internal>
2019-12-13 04:10:57 +0000

    Cure 2.5 (script location)

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,6 +4,60 @@
         <script src="/Cure/public/assets/js/jquery-3.4.1.min.js"></script>
         <script type="text/javascript" src="/Cure/public/assets/js/sign.js"></script>
         <script type="text/javascript" src="/Cure/public/assets/js/toggle.js"></script>
+<script>
+$('document').ready(function(){
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    //유효성 검사
+    $("#TP").on("click", function(){
+	    if( "{{session() -> has('id') ?? ''}}" == "") {
+		    alert("로그인 해주십시오.");
+	    } else {
+		    location.href="/Cure/public/join/list";
+	    }
+    })
+    //유효성 검사
+    $("#eventStart").on("click", function(){
+            if( "{{session() -> has('id') ?? ''}}" == "") {
+                    alert("로그인 해주십시오.");
+            } else {
+                    location.href="/Cure/public/festival";
+            }
+    })
+
+
+
+    $("#log").on("submit", function(){
+    	var id = $("input[name=id]").val();
+ 	var pw = $("input[name=password]").val();
+    	$.ajax({
+		url:"/Cure/public/member/ck",
+   		type:"post",
+    		data:{"id":id, "pw":pw},
+		success:function(data){
+			if(data == "noID") {
+				$("#warning").empty()
+				$("#warning").append("아이디를 입력해 주세요.");
+			} else if (data == "noPW") {
+				$("#warning").empty()
+				$("#warning").append("비밀번호를 입력해 주세요.");
+			} else if (data == "falseID" | data == 'falsePw') {
+				$("#warning").empty()
+				$("#warning").append("아이디 또는 비밀번호가 잘못되었습니다.");
+			} else if(data == "success") {
+				location.reload();
+			}		
+
+    		}
+    	});
+	return false;
+    })
+})
+</script>
         <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0"/>
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -88,59 +142,5 @@
 @yield('content')
 
 
-<script>
-$('document').ready(function(){
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    //유효성 검사
-    $("#TP").on("click", function(){
-	    if( "{{session() -> has('id') ?? ''}}" == "") {
-		    alert("로그인 해주십시오.");
-	    } else {
-		    location.href="/Cure/public/join/list";
-	    }
-    })
-    //유효성 검사
-    $("#eventStart").on("click", function(){
-            if( "{{session() -> has('id') ?? ''}}" == "") {
-                    alert("로그인 해주십시오.");
-            } else {
-                    location.href="/Cure/public/festival";
-            }
-    })
-
-
-
-    $("#log").on("submit", function(){
-    	var id = $("input[name=id]").val();
- 	var pw = $("input[name=password]").val();
-    	$.ajax({
-		url:"/Cure/public/member/ck",
-   		type:"post",
-    		data:{"id":id, "pw":pw},
-		success:function(data){
-			if(data == "noID") {
-				$("#warning").empty()
-				$("#warning").append("아이디를 입력해 주세요.");
-			} else if (data == "noPW") {
-				$("#warning").empty()
-				$("#warning").append("비밀번호를 입력해 주세요.");
-			} else if (data == "falseID" | data == 'falsePw') {
-				$("#warning").empty()
-				$("#warning").append("아이디 또는 비밀번호가 잘못되었습니다.");
-			} else if(data == "success") {
-				location.reload();
-			}		
-
-    		}
-    	});
-	return false;
-    })
-})
-</script>
 </body>
 </html>
